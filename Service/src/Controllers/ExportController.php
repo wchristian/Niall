58e3edf2e87471f1e4aabfafad40168cb888bc53
Matthew Baggett <matthew@baggett.me>
2016-09-14 19:37:56 +0100

    add stats page

--- Service/src/Controllers/ExportController.php
+++ Service/src/Controllers/ExportController.php
@@ -14,8 +14,18 @@ class ExportController extends Controller{
         return $response->withJson([
             "languages" => Models\NiallLanguage::search()->exec(),
             "words" => Models\NiallWord::search()->exec(),
-            "word_langauge" => Models\NiallWordLanguage::search()->exec(),
-            'word_relationship' => Models\NiallWordRelationship::search()->exec(),
+            "word_languages" => Models\NiallWordLanguage::search()->exec(),
+            'word_relationships' => Models\NiallWordRelationship::search()->exec(),
+            'response_time' => number_format(microtime(true) - APP_START_MICROTIME, 3) . " sec",
+        ]);
+    }
+
+    public function doStats(Request $request, Response $response, $args){
+        return $response->withJson([
+            "languages" => Models\NiallLanguage::search()->count(),
+            "words" => Models\NiallWord::search()->count(),
+            "word_languages" => Models\NiallWordLanguage::search()->count(),
+            'word_relationships' => Models\NiallWordRelationship::search()->count(),
             'response_time' => number_format(microtime(true) - APP_START_MICROTIME, 3) . " sec",
         ]);
     }
--- Service/src/Routes.php
+++ Service/src/Routes.php
@@ -7,7 +7,8 @@ $app->group("/v1", function () {
     $this->group("/ping", function () {
         $this->any("", \Niall\Controllers\PingController::class . ':doPing')->setName("Ping!");
     });
-    $this->map(["GET", "POST"], "/speak", \Niall\Controllers\ResponseController::class . ':doSpeak')->setName("Talk to the Guru");
-    $this->map(["POST"], "/listen", \Niall\Controllers\ResponseController::class . ':doListen')->setName("Talk to the Guru");
-    $this->get("/export", \Niall\Controllers\ExportController::class . ":doExport")->setName("Do export");
+    $this->map(["GET", "POST"], "/speak", \Niall\Controllers\ResponseController::class . ':doSpeak')->setName("Talk to the Guru and get a response.");
+    $this->map(["POST"], "/listen", \Niall\Controllers\ResponseController::class . ':doListen')->setName("Make the Guru Listen.");
+    $this->get("/export", \Niall\Controllers\ExportController::class . ":doExport")->setName("Do export as a hulking brick of JSON.");
+    $this->get("/stats", \Niall\Controllers\ExportController::class . ":doStats")->setName("Show Stats");
 });
